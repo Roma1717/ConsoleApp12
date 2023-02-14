5b3400dac6d9222026807917725d1123d65ddab0
Roma1717 <rzavtonev@gmail.com>
2023-02-14 19:50:58 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1
+            //int[] omas = new int[6];
+            //int i;
+            //for (i = 0; i <omas.Length; i++)
+            //{
+            //    Console.Write($"omas[{i}]=", i);
+            //    omas[i] = int.Parse(Console.ReadLine());
+            //}
+            //for (i = 0; i <omas.Length; i++)
+            //{
+            //    Console.Write(" " + omas[i]);
+            //}
+            // Задание 2
+            //int[] omas = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //int i;
+            //for(i = 0; i < omas.Length; i++)
+            //{
+            //    Console.WriteLine($"omas=[{i}]={omas[i]}");
+            //}
+            // Задание 3 
+            //int[] intArr;
+            //intArr = new int[6];
+            //foreach(int i in intArr)
+            //{
+            //    Console.Write(" " + intArr[i]);
+            //    intArr[i] = i+4;
+            //}
+            // Задание 4 
+            //Console.Write("Введите размерность массивов (от 5 до 20) ");
+            //int n = int.Parse(Console.ReadLine());
+            //Random r = new Random(); 
+            //int[] a = new int[n];
+            //foreach (int elem in a)
+            //{
+            //    if (elem < 0)
+            //    {
+            //        a[n] = r.Next(-20, 20); 
+            //        Console.Write("{0,4}", a[n]);
+            //    }
+            //}
+            Console.ReadKey();
+        }
+    }
+    
+}
